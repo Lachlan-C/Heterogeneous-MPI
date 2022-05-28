d2a04c60839f20a397d25c813f1a2e8623a24afb
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-05-28 18:11:23 +1000

    FIX: strings on remote server

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ do
     then
         # Copy executiable with name of ARCH and rename to executable
         scp MPI-CODE/$ARCH pi@${server}:/home/pi/Heterogeneous-MPI/MPI-CODE/C-Code
-        ssh pi@${server} 'mv Heterogeneous-MPI/MPI-CODE/$ARCH Heterogeneous-MPI/MPI-CODE/C-Code/$1'
+        ssh pi@${server} "mv Heterogeneous-MPI/MPI-CODE/$ARCH Heterogeneous-MPI/MPI-CODE/C-Code/$1"
 
     else
 
@@ -22,16 +22,16 @@ do
         scp MPI-CODE/C-Code/$1 pi@${server}:/home/pi
         
         #build code
-        ssh pi@${server} 'mpicc $1'
-        ssh pi@${server} 'mv a.out $1'
+        ssh pi@${server} "mpicc $1"
+        ssh pi@${server} "mv a.out $1"
         echo $ARCH
 
         #copy back to main node
-        ssh -A pi@${server} 'scp $ARCH pi@${main}:/home/pi/Heterogeneous-MPI/MPI-CODE'
+        ssh -A pi@${server} "scp $ARCH pi@${main}:/home/pi/Heterogeneous-MPI/MPI-CODE"
 
         #copy and rename locally to executable 
-        ssh pi@${server} 'mv $ARCH Heterogeneous-MPI/MPI-CODE/C-Code'
-        ssh pi@${server} 'mv $ARCH $1 | rev | cut -c 3- | rev'
+        ssh pi@${server} "mv $ARCH Heterogeneous-MPI/MPI-CODE/C-Code"
+        ssh pi@${server} "mv $ARCH $1 | rev | cut -c 3- | rev"
 
         #ADD to list
         KNOWN_ARCHS=$KNOWN_ARCHS" "$ARCH; 
