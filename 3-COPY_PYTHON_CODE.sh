3d232f04b30f99e64c3c6caa42b12f35c49bc1e1
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-05-28 17:15:05 +1000

    FIX: copy python

--- 3-COPY_PYTHON_CODE.sh
+++ 3-COPY_PYTHON_CODE.sh
@@ -5,4 +5,4 @@ tail=$(tail -n $num servers)
 all=$(cat servers)
 
 #copy over code to each of the worker nodes
-for server in $tail ; do scp MPI-CODE/Python-Code pi@${server}:/home/pi/Heterogeneous-MPI/MPI-CODE/Python-Code ; done
+for server in $tail ; do scp MPI-CODE/Python-Code/* pi@${server}:/home/pi/Heterogeneous-MPI/MPI-CODE/Python-Code ; done
