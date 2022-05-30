f9a3b4d43b3df0a04ed40f682269cfd826b4cb5d
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-05-30 18:15:05 +1000

    fix

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ for server in $all ;
 do 
     ARCH=$(ssh pi@${server} 'uname -a | rev | cut -d "' '" -f 2 | rev');
     echo $ARCH
-    filename=($1 | rev | cut -c 3- | rev)
+    filename=$1 | rev | cut -c 3- | rev
     echo $1 | rev | cut -c 3- | rev
     if (echo "$KNOWN_ARCHS" | fgrep -qw $ARCH);
     
@@ -17,30 +17,30 @@ do
         echo "old node detected"    
         # Copy executiable with name of ARCH and rename to executable
         echo "copy over compiled code"
-        scp MPI-CODE/$ARCH pi@${server}:/home/pi/Heterogeneous-MPI/MPI-CODE/C-Code
+        scp MPI-CODE/$ARCH pi@${server}:/home/pi/Heterogeneous-MPI/MPI-CODE/Compiled
         echo "rename compile"
-        ssh pi@${server} mv /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/$ARCH /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/$1
+        ssh pi@${server} mv /home/pi/Heterogeneous-MPI/MPI-CODE/Compiled/$ARCH /home/pi/Heterogeneous-MPI/MPI-CODE/Compiled/$1
 
     else
         echo "new node arch detected"
         #copy code
         echo "copy code to target machine"
-        scp MPI-CODE/C-Code/$1 pi@${server}:/home/pi/Heterogeneous-MPI
+        scp MPI-CODE/C-Code/$1 pi@${server}:/home/pi/Heterogeneous-MPI/MPI-CODE/C-Code
 
         
         #build code
         echo "build code on new node"
-        ssh pi@${server} cd /home/pi/Heterogeneous-MPI ; $(mpicc $1 -o $ARCH)
+        ssh pi@${server} cd /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code; $(mpicc $1 -o $ARCH)
         
         #copy back to main node
         echo "copy code back to main node"
-        ssh -A pi@${server} scp /home/pi/Heterogeneous-MPI/$ARCH pi@${main}:/home/pi/Heterogeneous-MPI/MPI-CODE
+        ssh -A pi@${server} scp /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/$ARCH pi@${main}:/home/pi/Heterogeneous-MPI/MPI-CODE
 
         #copy and rename locally to executable
         echo "Move compiled to correct place"
-        ssh pi@${server} mv /home/pi/Heterogeneous-MPI/$ARCH /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/
+        ssh pi@${server} mv /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/$ARCH /home/pi/Heterogeneous-MPI/MPI-CODE/Compiled/
         echo "rename compile"
-        ssh pi@${server} cd /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code ; mv /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/$ARCH /home/pi/Heterogeneous-MPI/MPI-CODE/C-Code/$filename
+        ssh pi@${server} cd /home/pi/Heterogeneous-MPI/MPI-CODE/Compiled ; mv /home/pi/Heterogeneous-MPI/MPI-CODE/Compiled/$ARCH /home/pi/Heterogeneous-MPI/MPI-CODE/Compiled/$filename
 
         #ADD to list
         KNOWN_ARCHS=$KNOWN_ARCHS" "$ARCH; 
