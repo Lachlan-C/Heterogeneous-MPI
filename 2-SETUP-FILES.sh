ff0b1d10e52d1e7ee3e0c2824d08528441bb82e5
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-05-30 18:45:10 +1000

    fix

--- 2-SETUP-FILES.sh
+++ 2-SETUP-FILES.sh
@@ -6,7 +6,7 @@ all=$(cat servers)
 
 #create requried directories on  each node
 for server in $tail ; do ssh pi@${server} 'mkdir -p ~/Heterogeneous-MPI/MPI-CODE/C-Code' ; done
-for server in $tail ; do ssh pi@${server} 'mkdir -p ~/Heterogeneous-MPI/MPI-CODE/Compiled' ; done
+for server in $all ; do ssh pi@${server} 'mkdir -p ~/Heterogeneous-MPI/MPI-CODE/Compiled' ; done
 for server in $tail ; do ssh pi@${server} 'mkdir -p ~/Heterogeneous-MPI/MPI-CODE/Python-Code' ; done
 
 #setup host file on eack worker node
