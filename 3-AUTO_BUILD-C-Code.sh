3d8641a6069e4c70ce8cb22f318a3bead86194c6
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-05-30 17:02:22 +1000

    test

--- 3-AUTO_BUILD-C-Code.sh
+++ 3-AUTO_BUILD-C-Code.sh
@@ -6,4 +6,5 @@ all=$(cat servers)
 code=$(ls MPI-CODE/C-Code)
 
 #Auto Build every C code file
-for file in $code ; do ./build.sh $file; done
+
+for file in $code ; do echo $file; echo $($file | rev | cut -c 3- | rev) ; ./build.sh $file; done
